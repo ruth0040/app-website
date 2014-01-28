f180ecd93eb3fb4811f132edc841dcdb6581237b
ruth0040 <beaker6591@Robyn-Rutherfords-MacBook-Pro.local>
2014-01-28 10:51:29 -0500

    video

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Carousel items
 
 var $items = $('.items img');
+var $videoDialog = $('dialog');
+dialogPolyfill.registerDialog($videoDialog.get(0));
 
 var switchItem = function (current, incoming) {
   $items.eq(incoming).attr('data-state', 'incoming').fadeIn(250, function () {
@@ -34,3 +36,31 @@ $('.previous').on('click', function () {
   switchItem(current, previous);
 
 });
+
+
+//  Video Box
+$('.btn-open').on('click', function () {
+  $videoDialog.children('.video').html('<iframe src="http://player.vimeo.com/video/81312446?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> ');
+
+  /* when triggering built in javascript functions we need to bypass jQuery in order to get to the raw javascript element we can use .get()
+
+  example: $videoDialog.show() - will tigger jQuerys show function not the raw Javascript show function
+
+        $videoDialog.get(0).show() - will get the first raw javascript element and trigger its native show function
+
+  */
+  $videoDialog.get(0).showModal();
+
+  /*
+  .show() allows users to interact with stuff behind the dialog
+  .showModal() disables user interact with stuff behind the dialog
+  */
+
+});
+
+$('.btn-close').on('click', function () {
+  $videoDialog.get(0).close();
+  $videoDialog.children('.video').html('');
+});
+
+
